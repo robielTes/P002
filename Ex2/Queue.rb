427d79eed0bfc86524b9debf198c8e7e5727ab31
robielcpnv <robiel.tesfazghi@cpnv.ch>
2022-05-05 08:35:25 +0200

    :start: add Queue class

---
+++ Ex2/Queue.rb
@@ -0,0 +1,22 @@
+class Queue
+    def initialize
+        @queue = []
+    end
+    
+    def enqueue(el)
+        @queue.push(el)
+    end
+    
+    def dequeue
+        @queue.shift
+    end
+
+    def size
+        @queue.size
+    end
+
+    def isEmpty?
+        @queue.empty?
+    end
+
+end
--- Ex2/main.rb
+++ Ex2/main.rb
@@ -1,42 +1,22 @@
-require_relative 'LinkedList'
-list = LinkedList.new
+require_relative 'Queue'
+list = Queue.new
+
+list.enqueue('L')
+list.enqueue('O')
+list.enqueue('O')
+list.enqueue('C')
+
+puts "List size: #{list.size}" # 4
+puts "List empty? #{list.isEmpty?}" # false
+
+puts "First element: #{list.dequeue}" # L
+puts "Second element: #{list.dequeue}" # O
+
+puts "List size: #{list.size}" # 2
+puts "List empty? #{list.isEmpty?}" # false
+
+puts "First element: #{list.dequeue}" # O
+puts "Second element: #{list.dequeue}" # C
 
 puts "List size: #{list.size}" # 0
 puts "List empty? #{list.isEmpty?}" # true
-
-list.addHead('L')
-list.addHead('O')
-list.addHead('O')
-list.addHead('C')
-
-puts "List size: #{list.size}" # 4
-puts "List empty? #{list.isEmpty?}" # false
-puts "First element: #{list.getHead()}" # C
-puts "Last element:  #{list.getTail()}" # L
-
-list.addTail('X')
-list.addTail('Y')
-
-puts "List size: #{list.size}" # 6
-puts "List empty? #{list.isEmpty?}" # false
-puts "First element: #{list.getHead()}" # C
-puts "Last element:  #{list.getTail()}" # Y
-
-puts "RemoveHead: #{list.removeHead()}"
-puts "RemoveHead: #{list.removeTail()}"
-
-puts "List size: #{list.size}" # 4
-puts "List empty? #{list.isEmpty?}" # false
-puts "First element: #{list.getHead()}" # O
-puts "Last element:  #{list.getTail()}" # X
-
-puts "You should see an empty list exception now"
-begin
-  puts "RemoveHead: #{list.removeHead}"
-  puts "RemoveHead: #{list.removeHead}"
-  puts "RemoveHead: #{list.removeHead}"
-  puts "RemoveHead: #{list.removeHead}"
-  puts "RemoveHead: #{list.removeHead}"
-rescue EmptyListError
-  puts "!!! The list is empty !!!"
-end
