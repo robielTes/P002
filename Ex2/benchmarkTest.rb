b178c64eee4266eff54a984fea429ff0d505d017
robielcpnv <robiel.tesfazghi@cpnv.ch>
2022-05-05 09:50:37 +0200

    :start: add benchmark test

---
+++ Ex2/benchmarkTest.rb
@@ -0,0 +1,36 @@
+require_relative 'ArrayQueue'
+require_relative 'ListQueue'
+require 'benchmark'
+
+smallArrayQueue = ArrayQueue.new
+smallListQueue = ListQueue.new
+
+largeArrayQueue = ArrayQueue.new
+largeListQueue = ListQueue.new
+
+def fillQueue(queue, size)
+    size.times do
+        queue.enqueue(rand(10000))
+    end
+end
+
+
+Benchmark.bmbm  do |x|
+    x.report("smallArrayQueue") do
+        fillQueue(smallArrayQueue, 100)
+    end           
+    x.report("smallListQueue") do
+        fillQueue(smallListQueue, 100)
+    end
+    x.report("largeArrayQueue") do
+        fillQueue(largeArrayQueue, 100000)
+    end       
+    x.report("largeListQueue") do
+        fillQueue(largeListQueue,100000)
+    end    
+end
+
+
+
+
+
